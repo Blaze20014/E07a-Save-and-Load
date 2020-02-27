f5814b39e8b4a3edaf32f9be42abbad13c439596
Blaze20014 <koutsokids1@gmail.com>
2020-02-26 23:11:17 -0500

    finished

--- Scenes/SaveSystem.gd
+++ Scenes/SaveSystem.gd
@@ -2,11 +2,17 @@ extends Node2D
 
 var DisplayValue = 0
 
+var savepath = "res://save-file.cfg"
+var config = ConfigFile.new()
+var load_response = config.load(savepath)
+
 func _ready():
 	pass 
 
-func saveValue():
-	pass
+func saveValue(section, key):
+	config.set_value(section, key, DisplayValue)
+	config.save(savepath)
 	
-func loadValue():
-	pass
+func loadValue(section, key):
+	DisplayValue = config.get_value(section, key, DisplayValue)
+	
